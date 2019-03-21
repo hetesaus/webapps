87129654cbf196a95b41e1f4e0a0dcef23ebadad
hetesaus <mhs@live.nl>
2019-03-21 13:50:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 var countdownList = 
 [
-	{ name: 'Christmas', date: '2019-12-25 00:00:00' }
+	{ name: 'Christmas', date: '2019-12-25 00:00:00' },
+	{ name: 'Sekiro', date: '2019-03-22 16:00:00' }
 ];
