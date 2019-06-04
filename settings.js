41ae4565a88a6d31d9a46b7e3d0d406af7fc534f
hetesaus <mhs@live.nl>
2019-06-04 14:44:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,11 @@
 var countdownList = 
 [
 	{ name: 'Christmas', date: '2019-12-25 00:00:00' },
+	{ name: 'E3 Microsoft', date : '2019-06-09 22:00:00' },
+	{ name: 'E3 Bethesda ', date : '2019-06-10 02:00:00' },
+	{ name: 'E3 PC Gaming Show', date : '2019-06-10 19:00:00' },
+	{ name: 'E3 Ubisoft', date : '2019-06-10 22:00:00' },
+	{ name: 'E3 Square Enix', date : '2019-06-11 03:00:00' },
+	{ name: 'E3 Nintendo', date : '2019-06-11 18:00:00' },
+	{ name: 'E3 Expo', date : '2019-06-11 21:00:00' }
 ];
